00f343c89079fa39af0aec0025c47e64aa693df8
Emilio <epelaaez@gmail.com>
2020-11-16 22:13:23 +0100

    Add greatest common divisor algorithm

---
+++ week2/greatest_common_divisor.py
@@ -0,0 +1,18 @@
+def greatest_common_divisor(a, b):
+    gcd = -1
+    
+    if a < 0 or b < 0:
+        return gcd
+    elif a == 0:
+        return b
+    elif b == 0:
+        return a
+
+    a_prime = a % b
+
+    return greatest_common_divisor(b, a_prime)
+
+if __name__ == '__main__':
+    input_a = int(input())
+    input_b = int(input())
+    print(greatest_common_divisor(input_a, input_b))

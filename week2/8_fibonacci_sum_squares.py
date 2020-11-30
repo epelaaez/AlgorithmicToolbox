0035bb71685a65a56bd2a02fdb642dc2977b7cf8
Emilio <epelaaez@gmail.com>
2020-11-30 13:01:03 +0100

    Finish problem 8 and week 2

--- week2/8_fibonacci_sum_squares.py
+++ week2/8_fibonacci_sum_squares.py
@@ -1,19 +1,31 @@
-from sys import stdin
+import sys
 
-def fibonacci_sum_squares_naive(n):
+def fibonacci_sum_squares(n):
     if n <= 1:
         return n
+    
+    n %= 60
+    answer = fibonacci_last_digit(n) * fibonacci_last_digit(n + 1)
+    while (answer >= 10):
+        answer %= 10
 
-    previous = 0
-    current  = 1
-    sum      = 1
+    return answer
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current * current
+def fibonacci_last_digit(n_number):
+    if n_number <= 1:
+        return n_number
+    
+    last_2 = 0
+    last_1 = 1
+    answer = 0
 
-    return sum % 10
+    for _ in range(2, n_number + 1):
+        answer = (last_2 + last_1)  % 10
+        last_2 = last_1
+        last_1 = answer
+        
+    return answer
 
 if __name__ == '__main__':
-    n = int(stdin.readline())
-    print(fibonacci_sum_squares_naive(n))
+    n = int(sys.stdin.readline())
+    print(fibonacci_sum_squares(n))

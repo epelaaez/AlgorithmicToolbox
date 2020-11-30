76516dcffc4727d15f08178f88357b7e3826ec58
Emilio <epelaaez@gmail.com>
2020-11-30 12:28:15 +0100

    Finish problem 6

--- week2/6_fibonacci_sum_last_digit.py
+++ week2/6_fibonacci_sum_last_digit.py
@@ -1,32 +1,19 @@
 import sys
 
 def fibonacci_sum(n):
-    answer = 0
-    
-    for i in range(n + 1):
-        answer += fibonacci_last_digit(i)
-    
-    while(answer >= 10):
-        answer %= 10
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
 
-    return answer
+    a, b = 0, 1
 
-def fibonacci_last_digit(n_number):
-    if n_number <= 1:
-        return n_number
-    
-    last_2 = 0
-    last_1 = 1
-    answer = 0
+    for i in range(n + 2):
+        a, b = b, a + b
 
-    for _ in range(2, n_number + 1):
-        answer = (last_2 + last_1)  % 10
-        last_2 = last_1
-        last_1 = answer
-        
-    return answer
+    return (a - 1) % 10
 
 if __name__ == '__main__':
     input = sys.stdin.readline()
     n = int(input)
-    print(fibonacci_sum(n))
+    print(fibonacci_sum(n % 60))

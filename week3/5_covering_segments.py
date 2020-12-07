b790e66a4b43c6d58e7a4c5ebfc04f80630a17a0
Emilio <epelaaez@gmail.com>
2020-12-07 13:56:28 +0100

    Solved assignemnt 5

--- week3/5_covering_segments.py
+++ week3/5_covering_segments.py
@@ -4,32 +4,14 @@ from collections import namedtuple
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
-    segments = sorted(segments, key = lambda s: s.start) # sort segments by starting point
-    points = []
+    segments = sorted(segments, key = lambda s: s.end) # sort segments by point point
+    current_point = segments[0].end
+    points = [current_point] 
 
-    while len(segments) > 0:
-        reached_segments = []
-        shortest_segment = segments[0]
-
-        for segment in segments:
-            if segment.start != segments[0].start:
-                break
-            if segment.end - segment.start <= shortest_segment.end - shortest_segment.start:
-                shortest_segment = segment
-        print(shortest_segment)
-        for segment in segments:
-            if segment.start <= shortest_segment.end:
-                reached_segments.append(segment)
- 
-        common_points = [*range(reached_segments[0].start, reached_segments[0].end + 1)]
-        for segment in reached_segments:
-            common_points = [point for point in [x for x in range(segment.start, segment.end + 1)] 
-                            if point in common_points]
-
-        print(common_points, reached_segments)
-        points.append(common_points[0])
-        for point in reached_segments:
-            segments.remove(point)
+    for segment in segments:
+        if current_point < segment.start:
+            current_point = segment.end
+            points.append(current_point)
 
     return points
 

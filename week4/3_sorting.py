973f987c7371b6dc6f84a8c72398c818b7d12b1c
Emilio <epelaaez@gmail.com>
2020-12-28 14:12:50 +0100

    Add stress tests

--- week4/3_sorting.py
+++ week4/3_sorting.py
@@ -11,12 +11,15 @@ def partition3(a, l, r):
             a[i], a[j] = a[j], a[i]
     a[l], a[j] = a[j], a[l]
 
-    for i in range(l, j + 1):
-        if a[i] == x:
-            m1 = i
-            break 
+    m1, m2 = j, j # Case where there is only one element equal to the pivot (itself)
 
-    return m1, j
+    while m1 > l and a[m1] == x:
+        m1 -= 1
+
+    while m2 < r and a[m2] == x:
+        m2 += 1
+
+    return m1 + 1, m2 - 1
 
 def partition2(a, l, r):
     x = a[l] # Pivot
@@ -39,7 +42,24 @@ def randomized_quick_sort(a, l, r):
     randomized_quick_sort(a, l, m1 - 1)
     randomized_quick_sort(a, m2 + 1, r)
 
+def stress_test(l, r):
+    while True:
+        a = []
+        for _ in range(l):
+            k = random.randint(0, r)
+            a.append(k)
+        a_copy = a.copy()
+        randomized_quick_sort(a, 0, l - 1)
+        if (a != sorted(a)):
+            print("Incorrect")
+            print(a_copy, a)
+            return
+        else:
+            print("Correct")
+            print(a_copy, a)
+
 if __name__ == '__main__':
+    # stress_test(1000, 500)
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     randomized_quick_sort(a, 0, n - 1)

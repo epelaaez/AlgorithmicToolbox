0bacfa4a20d56cce43d1b007cfd1665a333318be
Emilio <epelaaez@gmail.com>
2020-12-29 21:13:40 +0100

    Finish inversions problem

--- week4/4_inversions.py
+++ week4/4_inversions.py
@@ -1,19 +1,44 @@
 import sys
+# Good video on the topic: https://www.youtube.com/watch?v=7_AJfusC6UQ&index=14&list=PLXFMmlk03Dt7Q0xr1PIAriY5623cKiH7V
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
+def get_number_of_inversions(a):
+    solution = merge_sort(a) # Sorted array will be returned in reverse, but we only care about the count of inverse in this problem
+    return solution[0]
+
+def merge_sort(a):
+    if len(a) == 1:
+        return [0, a]
+
+    mid = int(len(a) / 2)
+
+    ordered_left  = merge_sort(a[0:mid])
+    ordered_right = merge_sort(a[mid:])
+
+    return merge(ordered_left, ordered_right)
+
+def merge(left, right):
+    count = left[0] + right[0]
+    left_array  = left[1]
+    right_array = right[1]
     
-    #write your code here
-    
-    return number_of_inversions
+    merged_ordered = []
+    while len(left_array) != 0 and len(right_array) != 0:
+        if left_array[0] > right_array[0]:
+            merged_ordered.append(left_array[0])
+            count += len(right_array)
+            left_array.pop(0)
+        else:
+            merged_ordered.append(right_array[0])
+            right_array.pop(0)
+
+    # The loop will sometimes end with remaining elements either on the left or right array, they are added to the merged array after in order
+    merged_ordered += left_array
+    merged_ordered += right_array
+
+    return [count, merged_ordered]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    print(get_number_of_inversions(a))

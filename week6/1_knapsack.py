671fe98305c68599983271d811dc8867a5340fd1
Emilio <epelaaez@gmail.com>
2021-01-26 18:31:08 +0100

    Solved knapsack problem

--- week6/1_knapsack.py
+++ week6/1_knapsack.py
@@ -1,12 +1,17 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    solution = [[0] * len(w) for _ in range(W + 1)]
+
+    for i in range(len(w)):
+        for j in range(W + 1):
+            solution[j][i] = solution[j][i - 1]
+            if w[i] <= j:
+                value = solution[j - w[i]][i - 1] + w[i]
+                if solution[j][i] < value:
+                    solution[j][i] = value
+    
+    return solution[-1][-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
